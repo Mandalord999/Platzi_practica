5f0d4e3cda9678e79d331667076df1eda0e3c6d6
unknown <mat.ramirezg@gmail.com>
2021-11-24 18:19:14 -0300

    Calculo descuento js/html v4

--- descuento.js
+++ descuento.js
@@ -2,9 +2,24 @@
 const descuento = 14; */
 
 function calculoPrecioDescuento(precio , descuento){
+    
     const porcentajePrecioDescuento = 100 - descuento;
     const precioConDescuento = (precio * porcentajePrecioDescuento) / 100;
     return precioConDescuento;
+
+}
+
+function ClickCalculoPrecioDescuento(){
+
+    const inputPrecio = document.getElementById("inputPrecio");
+    const precioValue = inputPrecio.value;
+
+    const inputDescuento = document.getElementById("inputDescuento");
+    const descuentoValue = inputDescuento.value;
+
+    const preConDescuento = calculoPrecioDescuento(precioValue,descuentoValue);
+    const resultado = document.getElementById("resultadoPrecio");
+    resultado.innerText = "El precio de su producto es "+preConDescuento;
 
 }
 
